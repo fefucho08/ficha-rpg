1b11fc2e9ccb24f449e180aaee90aa589e4bbf4f
fefucho08 <63268454+fefucho08@users.noreply.github.com>
2023-08-22 17:23:51 -0300

    Initial commit

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import './header.css';
+
+export default function Header(){
+    return(
+        <div className="container">
+            <h1>Ficha do Investigador</h1>
+            <button>Resetar</button>
+        </div>
+    );
+}
