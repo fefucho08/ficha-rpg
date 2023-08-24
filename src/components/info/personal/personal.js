1729cb45c28b7265b1843fae203fbb3e535f1b61
fefucho08 <63268454+fefucho08@users.noreply.github.com>
2023-08-24 12:55:55 -0300

    feat: add input nex

--- src/components/info/personal/personal.js
+++ src/components/info/personal/personal.js
@@ -1,32 +1,44 @@
 import '../info.css';
 
-function Dados({info, id}){
+function Dados({info, id, tipo}){
+
+
     return(
-        <div>
+        <div className='inputContainer'>
             <label htmlFor={id}>{info}: </label>
             <input
                 autoComplete='off'
                 placeholder={info}
                 id={id}
+                type={tipo}
             />
         </div>
     )
 }
 
 export default function Personal(){
+
     return(
         <div className="box" id='personal'>
             <Dados
             info="Nome"
             id='nome'
+            tipo='text'
             />
             <Dados
             info="Origem"
             id='origem'
+            tipo='text'
             />
             <Dados
             info="Classe"
             id='classe'
+            tipo='text'
+            />
+            <Dados
+            info='Nex'
+            id='nex'
+            tipo='number'
             />
         </div>
     );
