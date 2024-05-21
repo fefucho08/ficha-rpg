5ccf6925a28383a5f5dfa6e08a43ea1c1e5d90b9
Felipe Ferreira <fefucho08@gmail.com>
2024-05-21 14:58:40 -0300

    fix: dados personalizados com rolagem infinita

--- src/components/sheetPage/extra/customDices/diceRoll.jsx
+++ src/components/sheetPage/extra/customDices/diceRoll.jsx
@@ -1,50 +1,55 @@
-import { useEffect, useState } from 'react'
-import { FaRegTimesCircle } from 'react-icons/fa'
-import validator from '../../../../validator'
-import '../extra.css'
+import { useEffect, useState, useRef } from "react";
+import { FaRegTimesCircle } from "react-icons/fa";
+import validator from "../../../../validator";
+import "../extra.css";
 
-export default function CustomDiceRoll(props){
-    const {value, name, isDamage, isRolling} = props
-    
-    const [valid, isValid] = useState(false)
+export default function CustomDiceRoll(props) {
+    const { value, name, isDamage, isRolling } = props;
 
-    const validation = validator(value)
+    const [valid, isValid] = useState(false);
+    const [result, setResult] = useState(0);
+    const [rolls, setRolls] = useState([]);
+
+    const validation = useRef(validator(value));
+    const isDamageRef = useRef(isDamage);
 
     useEffect(() => {
-        if(!validation){
-            isValid(false)
-            isRolling(false)
+        if (!validation.current) {
+            isValid(false);
+            isRolling(false);
+        } else {
+            isValid(true);
+            let newRolls = [];
+            let result = 0;
+
+            for (let i = 0; i < validation.current.multiplier; i++) {
+                let newRoll =
+                    Math.floor(Math.random() * validation.current.dice) + 1;
+                newRolls.push(newRoll);
+                if (isDamageRef.current) {
+                    result += newRoll;
+                } else {
+                    result = Math.max(result, newRoll);
+                }
+            }
+
+            result += validation.current.bonus;
+
+            setResult(result);
+            setRolls(newRolls);
         }
-        else
-            isValid(true)
-    }, [validation, isRolling])
-
-    let rolls = [];
-
-    for(let i = 0; i < validation.multiplier; i++){
-        let newRoll = Math.floor(Math.random()*validation.dice)+1
-        rolls.push(newRoll)
-    }
-
-    let result = 0;
-
-    if(isDamage){
-        for(let i = 0; i < rolls.length; i++)
-        result += rolls[i];
-    } else {
-        result = Math.max(...rolls);
-    }
-
-    result += validation.bonus
+    }, [value, isRolling]);
 
     return valid ? (
         <div className="popUp">
             <span onClick={() => isRolling(false)}>
-                <FaRegTimesCircle/>
+                <FaRegTimesCircle />
             </span>
             <h2>Rolagem de {name}</h2>
             <h3>{result}</h3>
             <p>Os resultados foram: {rolls.join(", ")}</p>
         </div>
-    ) : ""
+    ) : (
+        ""
+    );
 }
